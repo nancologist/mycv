ec45f2e86d1fafc6a7860ce0223d1b4fe795319d
Morteza Jalilifar <morteza.jalilifar@cgm.com>
2024-05-22 11:57:34 +0200

    Update skills and links

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -14,7 +14,7 @@ const Main = (props) => {
 
       <div className="section profile">
         <h2>Profile</h2>
-        <p>Fullstack engineer with {new Date().getFullYear() - 2019}+ years of hands-on experience designing, developing and implementing the applications, solutions and quality assurance using a wide range of technologies and programming languages.</p>
+        <p>Fullstack engineer with {new Date().getFullYear() - 2019} years of hands-on experience designing, developing and implementing the applications, solutions and quality assurance using a wide range of technologies and programming languages.</p>
         <p>Using best practices and latest design patterns to improve the code quality.</p>
         <p>A team player, worked in big teams using agile project methods and tools. Disciplined and following team rules and clean codes. Postivie technical discussions with other team members.</p>
         <p>Enthusiast to learn new technologies and making my knowledge deeper in technologies which I have used over the years.</p>
