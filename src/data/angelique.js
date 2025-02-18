df3326268d65f7967b39e57da53d781ff0fd269e
Morteza Jalilifar <morteza.jalilifar@netgo.de>
2025-02-18 23:38:48 +0100

    wip

---
+++ src/data/angelique.js
@@ -0,0 +1,235 @@
+export const workXps = [
+    {
+        title: "Elternzeit",
+        from: "Okt 2020",
+        to: "heute"
+    },
+    {
+        title: "Erzieherin an der Grunewald-Grundschule",
+        from: "Sep 2019",
+        to: "Sep 2020"
+    },
+    {
+        title: "Museumspädagogische Mitarbeiterin (Gruppenführungen mit Kita- und Grundschulkindern)",
+        name: "ALICE - Museum für Kinder im FEZ-Berlin",
+        from: "2016",
+        to: "2019",
+        details: [
+            "Regelmäßige, gründliche und zügige Einarbeitung in neue Themengebiete der Ausstellung",
+            "Vor- und Nachbereitung des täglichen Ausstellungsbetriebs",
+            "Betreuung der Familienbesucher*innen in der Ausstellung an Wochenenden und in den Ferien",
+            "Moderation der Ausstellung für Kindergartengruppen Schulklassen sowie Gruppen von Auszubildenden und Studierenden",
+            "Durchführung von thematischen Sonderveranstaltungen im Format Familien-Kreativ-Sonntag"
+        ]
+    },
+    {
+        title: "Darstellerin und Teamleiter im partizipativen Theaterprojekt \"Zauberschloss\"",
+        name: "FEZ-Berlin",
+        from: "Jan 2015",
+        to: "Feb 2018"
+    },
+    {
+        title: "Mitarbeit und Moderation bei FEZitty",
+        name: "FEZ-Berlin",
+        from: "Jul 2015",
+        to: "Aug 2015"
+    },
+    {
+        title: "Pädagogische Honorartätigkeit (Gruppenleitung spielpädagogische Projekte mit Familien)",
+        name: "FEZ-Berlin",
+        from: "Feb 2015",
+        to: "Jun 2019"
+    },
+    {
+        title: "Hausaufgabenbetreuung",
+        name: "Grundschule Grünau Leipzig",
+        from: "Jan 2013",
+        to: "Aug 2013"
+    },
+    {
+        title: "Ehrenamt: Vorlesepatin",
+        name: "Kita Outlaw - Leipzig",
+        from: "Jan 2013",
+        to: "Aug 2013"
+    },
+    {
+        title: "Tanzvertretungslehrerin",
+        name: "flatback-and-cry e.V.",
+        from: "Feb 2011",
+        to: "Feb 2011"
+    },
+    {
+        title: "Bürotätigkeit",
+        name: "flatback-and-cry e.V.",
+        from: "Jan 2011",
+        to: "Feb 2012"
+    }
+]
+
+export const educations = [
+    {
+        title: "Fernstudium der Ernährungsberatung",
+        name: "SGD Fernschule",
+        from: "MÄR 2020",
+        to: "MAI 2024"
+    },
+    {
+        "title": "Studium der Sozialen Arbeiten",
+        "name": "Alice-Salomon-Hochschule",
+        "from": "Okt 2015",
+        "to": "Jun 2019"
+    },
+    {
+        "title": "Studium der Germanistik/Grundschulpädagogik",
+        "name": "Universität Leipzig",
+        "from": "2012",
+        "to": "2014 "
+    },
+    {
+        "title": "deutsch-russisch bilinguales Abitur",
+        "name": "Johann-Gottfried-Herder Gymnasium",
+        from: "2012",
+        to: "2012"
+    }
+]
+
+export const internships = [
+    {
+        title: "Praktikum im Rahmen des Studiums",
+        name: "Theaterpädagogischen, sozio-kulturelle Einrichtung Kreativhaus",
+        from: "Okt 2017",
+        to: "Feb 2018"
+    },
+    {
+        from: "Mär 2016",
+        to: "Apr 2016",
+        title: "Praktikum in der rechtlichen Betreuung bei Peter Schmelzer"
+    },
+    {
+        from: "Mai 2015",
+        title: "Praktikum in der logopädischen Praxis Sprechmund Berlin"
+    },
+    {
+        from: "Sep 2014",
+        to: "Feb 2015",
+        title: "Praktikum Spiel- und Kulturpädagogik",
+        name: "FEZ-Berlin"
+    }
+]
+
+// export const certifications = [
+//     {
+//         "title": "AWS",
+//         "name": "8hrs - A Complete Introduction (Maximilian Schwarzmüller)",
+//         "from": "Oct 2021",
+//         "to": "Jan 2022",
+//         "details": [
+//             "Lambda, API Gateway, DynamoDB, Cognito",
+//             "S3, CloudFront, Route53"
+//         ]
+//     },
+//     {
+//         "title": "Docker",
+//         "name": "20hrs - Docker Mastery (Bret Fisher)",
+//         "from": "Jan 2021",
+//         "to": "Juli 2021",
+//         "details": [
+//             "Lambda, API Gateway, DynamoDB, Cognito",
+//             "S3, CloudFront, Route53"
+//         ]
+//     },
+//     {
+//         "title": "TypeScript",
+//         "name": "15hrs - Understanding Typescript (Maximilian Schwarzmüller)",
+//         "from": "Nov 2020",
+//         "to": "Dec 2020"
+//     },
+//     {
+//         "title": "NodeJS",
+//         "name": "40hrs - The Complete Guide (Maximilian Schwarzmüller)",
+//         "from": "Apr 2020",
+//         "to": "Jun 2020",
+//         "details": [
+//             "ExpressJS, MVC, EJS, Sequelize, Mongoose, Validators",
+//             "REST API, Pagination, Socket.io, GraphQL, NodeJS+TS"
+//         ]
+//     },
+//     {
+//         "title": "ReactJS",
+//         "name": "40hrs - The Complete Guide (Maximilian Schwarzmüller)",
+//         "from": "Mar 2020",
+//         "to": "May 2020",
+//         "details": [
+//             "Redux, React Router, Hooks, ...",
+//         ]
+//     },
+//     {
+//         "title": "VueJS",
+//         "name": "30hrs - The Complete Guide (Maximilian Schwarzmüller)",
+//         "from": "Jan 2020",
+//         "to": "Mar 2020",
+//         "details": [
+//             "VueRouter, Vuex, ..."
+//         ]
+//     }
+// ]
+
+// ####################################
+// ############ SIDE: #################
+// ####################################
+
+export const myLinks = [
+    // {
+    //   "label": "Website",
+    //   "link": "https://www.nancologist.com"
+    // },
+    // {
+    //   "label": "GitHub",
+    //   "link": "https://github.com/nancologist"
+    // },
+    {
+        "label": "LinkedIn",
+        "link": "https://www.linkedin.com/nancologist"
+    },
+    // {
+    //   "label": "CodeWars",
+    //   "link": "https://www.codewars.com/users/nancologist"
+    // }
+]
+
+export const skills = [
+    {
+        "scope": "Frontend",
+        "techs": [
+            {
+                "name": "Angular/TS",
+                "level": 4
+            },
+            {
+                "name": "ReactJS",
+                "level": 3
+            },
+            {
+                "name": "VueJS",
+                "level": 3
+            },
+        ]
+    },
+    {
+        "scope": "Backend",
+        "techs": [
+            {
+                "name": "NodeJS (NestJS)",
+                "level": 4
+            },
+            {
+                "name": "Java (Spring)",
+                "level": 3
+            },
+            {
+                "name": "Python (Sanic, FastAPI, Flusk)",
+                "level": 3
+            },
+        ]
+    }
+]
